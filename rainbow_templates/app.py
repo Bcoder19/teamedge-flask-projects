f4c2d267092f2a24ac0b13c857552ceb9addd804
Bcoder19 <bolivares2022@columbiasecondary.org>
2021-01-23 12:48:57 -0500

    Rainbow project

---
+++ rainbow_templates/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, current_app as current_app
+app = Flask(__name__)
+
+
+
+
+@app.route('/')
+def index():
+    return "Welcome to Brios's Rainbow Project!"
+
+
+@app.route('/red')
+def red():
+    color = "red"
+    return render_template('color.html', color= color)
+
+
+@app.route('/orange')
+def orange():
+    color = "orange"
+    return render_template('color.html', color= color)
+
+
+@app.route('/yellow')
+def yellow():
+    color = "yellow"
+    return render_template('color.html', color= color)
+
+@app.route('/green')
+def green():
+    color = "green"
+    return render_template('color.html', color= color)
+
+@app.route('/blue')
+def blue():
+    color = "blue"
+    return render_template('color.html', color= color)
+
+@app.route('/indigo')
+def indigo():
+    color = "indigo"
+    return render_template('color.html', color= color)
+
+@app.route('/violet')
+def violet():
+    color = "violet"
+    return render_template('color.html', color= color)
+
+
+@app.route('/rainbow')
+def rainbow():
+    return 
+
+if __name__ == '__main__':
+    app.run(debug = True, host ='0.0.0.0')
